43e9cf2f3aef287a01cdbbfa99aeeb551770baa8
franfan <47168232+franfan@users.noreply.github.com>
2019-02-22 21:15:08 -0800

    Update bigger-margins.js

--- bigger-margins.js
+++ bigger-margins.js
@@ -1,14 +1,11 @@
 function setup() {
-  createCanvas(600, 200);
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(255);
-  fill(0, 0, 255);
-  noStroke();
 
-  // Draws five circles on the screen, spaced 100 pixels apart
-  for (var i = 100; i < 600; i += 100) {
-    ellipse(i, 100, 80);
+  for (var x = 10; x < width-10; x = x + 10) {
+    line(x+90, height/2, mouseX, mouseY);
   }
 }
